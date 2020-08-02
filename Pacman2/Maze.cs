6d0449b751f7c4df2d4929d6ec36ad5f66e074f9
Cindy Cai <cindymxcai@gmail.com>
2020-08-03 11:16:32 +1200

    populated maze

--- Pacman2/Maze.cs
+++ Pacman2/Maze.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Pacman2
 {
@@ -8,7 +9,22 @@ namespace Pacman2
         {
             Height = mazeData.Count;
             Width = mazeData[0].Length;
+            MazeArray = new ITile[Height, Width];
+
+            var x = 0;
+            foreach (var lineData in mazeData)
+            {
+                var y = 0;
+                foreach (var tileType in lineData.Select(Parser.GetTileType))
+                {
+                    MazeArray[x, y] = tileType;
+                    y++;
+                }
+                x++;
+            }
         }
+
+        public ITile[,] MazeArray { get; set; }
 
         public int Width { get; set; }
 
--- PacmanTest/MazeTests.cs
+++ PacmanTest/MazeTests.cs
@@ -16,5 +16,25 @@ namespace PacmanTest
             Assert.Equal(21, maze.Height);
             Assert.Equal(19, maze.Width);
         }
+
+        [Fact]
+        public void GivenMazeSizeShouldCreateArrayOfTiles()
+        {
+            var mazeData = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "mazeData.txt"));
+            var maze = new Maze();
+            maze.CreateMaze(mazeData);
+            Assert.Equal(399, maze.MazeArray.Length);
+        }
+
+        [Fact]
+        public void GivenSizeAndDataShouldPopulateMazeArray()
+        {
+            var mazeData = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "mazeData.txt"));
+            var maze = new Maze();
+            maze.CreateMaze(mazeData);
+            Assert.Equal(" . ", maze.MazeArray[0,0].Display);
+            Assert.Equal(" * ", maze.MazeArray[0,6].Display);
+
+        }
     }
 }
